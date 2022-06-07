79df327b12943e37d14f2a8be90c2c04f59f0979
Haisch-Nico <nico.haisch@studmail.htw-aalen.de>
2022-06-07 16:44:53 +0200

    refactor: Changed Aufg_3.java

--- code/Aufg_3.java
+++ code/Aufg_3.java
@@ -9,12 +9,14 @@ import java.util.Scanner;
 
 public class Aufg_3 {
     public static void main(String[] args) throws Exception{
-        System.out.println("Es wird ein Schlüsselpaar generiert...");
+        System.out.println("Es wird ein Schlüsselpaar "
+		+"generiert...");
         KeyPair keypair = generateKeyPair();
         System.out.println("Schlüsselpaar generiert!");
         int i = 0;
         while(i==0){
-            System.out.println("Geben Sie einen zu verschlüsselnden Text ein:");
+            System.out.println("Geben Sie einen zu" 
+			+ " verschlüsselnden Text ein:");
 		    Scanner Text = new Scanner(System.in);
 		    String text=Text.nextLine();
 		    System.out.println("Der Text wird verschlüsselt...");
@@ -23,19 +25,21 @@ public class Aufg_3 {
             System.out.println("Verschlüssselte Nachricht:");
 		    System.out.println(CipherText);
 
-        
 		    Scanner decission = new Scanner(System.in);
-		
-        
+
             int k=0;
             while(k==0){ 
-                System.out.println("Soll der veschlüsselte Text auch wieder entschlüsselt werden? (j/n)");
+                System.out.println("Soll der veschlüsselte Text"
+				+ " auch wieder entschlüsselt werden? (j/n)");
                 String answer=decission.nextLine();   
 		        switch (answer) {
 		    	    case "j":
-                        System.out.println("Der Text wird nun wieder entschlüsselt...");
-			    	    String decipheredText=decrypt(CipherText,keypair);
-                        System.out.println("Entschlüssselte Nachricht:");
+                        System.out.println("Der Text wird nun"
+						+ " wieder entschlüsselt...");
+			    	    String decipheredText=
+						decrypt(CipherText,keypair);
+                        System.out.println("Entschlüssselte"
+						+ " Nachricht:");
                         System.out.println(decipheredText);
                         k=1;
                         break;
@@ -43,12 +47,14 @@ public class Aufg_3 {
                         k=1;
                         break;
 			        default:
-				        System.out.println("Falsche Eingabe! Benutzung: j/n");
+				        System.out.println("Falsche Eingabe!" 
+						+ " Benutzung: j/n");
 		        }
             }
             int j = 0;
             while(j==0){
-                System.out.println("Wollen sie noch einen Text verschlüsseln? (j/n)");
+                System.out.println("Wollen sie noch einen Text"
+				+ " verschlüsseln? (j/n)");
                 String answer=decission.nextLine();
 
                 switch (answer) {
@@ -62,7 +68,8 @@ public class Aufg_3 {
                         break;
                     default:
                         i=1;
-                        System.out.println("Falsche Eingabe! Benutzung: j/n");
+                        System.out.println("Falsche Eingabe!"
+						+ " Benutzung: j/n");
                         j=0;
                 }
             }
@@ -71,16 +78,17 @@ public class Aufg_3 {
         System.exit(0);
     }
     
-
     public static KeyPair generateKeyPair() throws Exception{
 		int keysize=2000;
-		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+		KeyPairGenerator keyPairGen = 
+		KeyPairGenerator.getInstance("RSA");
 		keyPairGen.initialize(keysize);
 		KeyPair rsaKeyPair = keyPairGen.generateKeyPair ();
         return rsaKeyPair;
     }
 
-    public static String encrypt(String text, KeyPair keypair) throws Exception{
+    public static String encrypt(String text, KeyPair keypair) 
+	throws Exception{
         String plainText = text;
 		PublicKey publicKey = keypair.getPublic ();
 		Cipher rsa = Cipher.getInstance("RSA");
@@ -91,7 +99,8 @@ public class Aufg_3 {
         return message;
     }
 
-    public static String decrypt(String cipherText, KeyPair keypair) throws Exception{
+    public static String decrypt
+	(String cipherText, KeyPair keypair) throws Exception{
 		Base64.Decoder decoder = Base64.getDecoder();
 		byte[] a = decoder.decode(cipherText);
         Cipher rsa = Cipher.getInstance("RSA");
