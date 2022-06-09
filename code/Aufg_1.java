e8930fe97fa420e884ff73ad37cfd5769653d28e
Moritz Hausmann <103260415+mrtzhsmnn@users.noreply.github.com>
2022-06-09 14:14:18 +0200

    refactor: Made linebreaks

--- code/Aufg_1.java
+++ code/Aufg_1.java
@@ -12,28 +12,35 @@ public class Aufg_1 {
         System.out.println("Wählen sie einen Operationsmodus: ");
         System.out.println("    1. SHA-Signatur erstellen");
         System.out.println("    2. SHA-Signatur validieren");
-        System.out.println("Geben Sie ihre Auswahl als Nummer ein: ");
+        System.out.println("Geben Sie ihre Auswahl als Nummer " +
+                "ein: ");
         try {
             a = mode.nextInt();
         } catch (Exception e) {
-            System.out.println("Ungültige Eingabe! Geben sie eine Zahl, von den oben gezeigten ein!");
+            System.out.println("Ungültige Eingabe! Geben sie " +
+                    "eine Zahl, von den oben gezeigten ein!");
             return;
         }
         switch (a) {
             case 1 -> {
-                System.out.println("Bitte geben Sie einen Text ein:");
+                System.out.println("Bitte geben Sie einen Text" +
+                        " ein:");
                 String message = in.nextLine();
                 System.out.println("Message: " + message);
                 try {
-                    System.out.println("Digest.: " + sha256(message));
+                    System.out.println("Digest.: "
+                            + sha256(message));
                 } catch (Exception e) {
-                    System.out.println("Error: " + e.getMessage());
+                    System.out.println("Error: "
+                            + e.getMessage());
                 }
             }
             case 2 -> {
-                System.out.println("Bitte geben Sie einen Text ein:");
+                System.out.println("Bitte geben Sie einen Text" +
+                        " ein:");
                 String tovalidate = in.nextLine();
-                System.out.println("Geben Sie nun die Signatur ein:");
+                System.out.println("Geben Sie nun die Signatur" +
+                        " ein:");
                 String sigin = in.nextLine();
                 String sigcalc = "";
                 try {
@@ -42,13 +49,17 @@ public class Aufg_1 {
                     System.out.println("Error: " + e.getMessage());
                 }
                 if (sigin.equals(sigcalc)) {
-                    System.out.println("Die Signatur zu der Nachricht: " + tovalidate);
+                    System.out.println("Die Signatur zu der " +
+                            "Nachricht: " + tovalidate);
                     System.out.println("ist korrekt!");
                 } else {
-                    System.out.println("Ihre übergebene Signatur ist ungültig.");
-                    System.out.println("Ihre Signatur zu der Nachricht: " + tovalidate);
+                    System.out.println("Ihre übergebene Signatur ist" +
+                            " ungültig.");
+                    System.out.println("Ihre Signatur zu der " +
+                            "Nachricht: " + tovalidate);
                     System.out.println("lautete: " + sigin);
-                    System.out.println("Die korrekte Signatur lautet: " + sigcalc);
+                    System.out.println("Die korrekte Signatur" +
+                            " lautet: " + sigcalc);
                 }
             }
             default -> System.out.println("Ungültige Eingabe!");
@@ -56,7 +67,8 @@ public class Aufg_1 {
     }
 
     public static String sha256(String message) throws Exception{
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        MessageDigest md = MessageDigest.
+                getInstance("SHA-256");
         byte[] digest = md.digest(message.getBytes ());
         return toHexString(digest, 2);
     }
@@ -67,7 +79,8 @@ public class Aufg_1 {
         StringBuilder sb = new StringBuilder ();
         for (int i=0; i<data.length; i++) {
             sb.append(String.format("%02X", data[i]));
-            if ((offset >0) && (i+1<data.length) && ((i+1) % offset == 0)) {
+            if ((offset >0) && (i+1<data.length)
+                    && ((i+1) % offset == 0)) {
                 sb.append(" ");
             }
         }
