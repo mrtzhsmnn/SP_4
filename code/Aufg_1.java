78e8e8e16d127ed13af93ab79eb0cf9a7535f3f2
Moritz Hausmann <103260415+mrtzhsmnn@users.noreply.github.com>
2022-06-30 00:04:25 +0200

    refactor: added comments, for maximum code-reading pleasure

--- code/Aufg_1.java
+++ code/Aufg_1.java
@@ -6,53 +6,77 @@ import java.util.*;
 
 public class Aufg_1 {
     public static void main(String[] args) {
+        // neue int Variable für die Eingabe erzeugen
         int a;
+        // Scanner Objekte für die Eingabe erzeugen
         Scanner mode = new Scanner(System.in);
         Scanner in = new Scanner(System.in);
+        // Benutzer Erklärungen
         System.out.println("Wählen sie einen Operationsmodus: ");
         System.out.println("    1. SHA-Signatur erstellen");
         System.out.println("    2. SHA-Signatur validieren");
         System.out.println("Geben Sie ihre Auswahl als Nummer " +
                 "ein: ");
+        // try-catch-Block für die Eingabe
         try {
+            // eingabe des modus in die Variable a schreiben
             a = mode.nextInt();
         } catch (Exception e) {
+            // Fehlermeldung ausgeben
             System.out.println("Ungültige Eingabe! Geben sie " +
                     "eine Zahl, von den oben gezeigten ein!");
             return;
         }
+        // switch-case-Block für die verschiedenen Modi
         switch (a) {
+            // Modus 1: SHA-Signatur erstellen
             case 1 -> {
+                // Ausgaben für den Benutzer
                 System.out.println("Bitte geben Sie einen Text" +
                         " ein:");
+                // Nachricht einlesen ...
                 String message = in.nextLine();
+                // ... und ausgeben
                 System.out.println("Message: " + message);
+                // try catch Block für die SHA-Signatur
                 try {
+                    // SHA-Signatur erstellen
                     System.out.println("Digest.: "
                             + sha256(message));
                 } catch (Exception e) {
+                    // Fehlermeldung ausgeben
                     System.out.println("Error: "
                             + e.getMessage());
                 }
             }
+            // Modus 2: SHA-Signatur validieren
             case 2 -> {
+                // Ausgaben für den Benutzer
                 System.out.println("Bitte geben Sie einen Text" +
                         " ein:");
+                // Die zu signierende Nachricht einlesen
                 String tovalidate = in.nextLine();
                 System.out.println("Geben Sie nun die Signatur" +
                         " ein:");
+                // Die alte Signatur einlesen
                 String sigin = in.nextLine();
                 String sigcalc = "";
+                // try catch Block für die SHA-Signatur
                 try {
+                    // SHA-Signatur erstellen, für den zu validierenden String
                     sigcalc = sha256(tovalidate);
                 } catch (Exception e) {
+                    // Fehlermeldung ausgeben
                     System.out.println("Error: " + e.getMessage());
                 }
+                // entspricht die neu generierte Signatur der alten?
                 if (sigin.equals(sigcalc)) {
+                    // Ausgabe für den Benutzer, dass Signatur gültig ist
                     System.out.println("Die Signatur zu der " +
                             "Nachricht: " + tovalidate);
                     System.out.println("ist korrekt!");
                 } else {
+                    // Ausgabe für den Benutzer, dass Signatur ungültig ist
                     System.out.println("Ihre übergebene " +
                             "Signatur ist ungültig.");
                     System.out.println("Ihre Signatur zu der " +
@@ -62,29 +86,38 @@ public class Aufg_1 {
                             " lautet: " + sigcalc);
                 }
             }
+            // Fehlermeldung ausgeben, wenn der Modus nicht existiert
             default -> System.out.println("Ungültige Eingabe!");
         }
     }
-
+    // Methode zur SHA-Signatur erstellen
     public static String sha256(String message) throws Exception{
+        // neue MessageDigest-Objekt erzeugen
         MessageDigest md = MessageDigest.
                 getInstance("SHA-256");
+        // Nachricht in Bytes umwandeln
         byte[] digest = md.digest(message.getBytes ());
+        // Bytes in Hex-String umwandeln
         return toHexString(digest, 2);
     }
-
+    // Methode zur Umwandlung von Bytes in Hex-Strings
     protected static String toHexString(byte[] data, int offset) {
+        // offset prüfen
         if (offset <0) {
             offset =0;
         }
+        // neues Stringbuilder Objekt erzeugen
         StringBuilder sb = new StringBuilder ();
+        // alle Bytes in Hex-Strings umwandeln
         for (int i=0; i<data.length; i++) {
+            // Hex-String erzeugen
             sb.append(String.format("%02X", data[i]));
             if ((offset >0) && (i+1<data.length)
                     && ((i+1) % offset == 0)) {
                 sb.append(" ");
             }
         }
+        // Hex-String zurückgeben
         return sb.toString ();
     }
 }
--- code/Aufg_2.java
+++ code/Aufg_2.java
@@ -11,9 +11,12 @@ import java.util.Scanner;
 
 public class Aufg_2 {
     public static void main(String[] args) {
+        // int a als Speicher für modus
         int a;
+        // neue Scanner Objekte für Eingabe
         Scanner mode = new Scanner(System.in);
         Scanner in = new Scanner(System.in);
+        // Ausgaben für den Benutzer
         System.out.println("Wählen sie einen Operationsmodus: ");
         System.out.println("    1. Mit AES-Verschlüsseln");
         System.out.println("    2. Mit AES-Verschlüsseln, aber " +
@@ -21,105 +24,164 @@ public class Aufg_2 {
         System.out.println("    3. Mit AES-Entschlüsseln");
         System.out.println("Geben Sie ihre Auswahl als Nummer " +
                 "ein: ");
+        // try-catch-Block für die Eingabe
         try {
+            // eingabe des modus in die Variable a schreiben
             a = mode.nextInt();
         } catch (Exception e) {
+            // Fehlermeldung ausgeben
             System.out.println("Ungültige Eingabe! Geben sie" +
                     " eine Zahl, von den oben gezeigten ein!");
             return;
         }
+        // switch-case-Block für die verschiedenen Modi
         switch (a) {
+            // Modus 1: AES-Verschlüsseln
             case 1 -> {
+                // Ausgaben für den Benutzer
                 System.out.println("Bitte geben Sie einen " +
                         "Klartext ein:");
+                // Klartext einlesen
                 String message = in.nextLine();
+                // Ausgaben für den Benutzer
                 System.out.println("Bitte geben Sie Ihren " +
                         "Schlüssel ein:");
+                // Schlüssel einlesen
                 String key = in.nextLine();
+                // Ausgaben für den Benutzer
                 System.out.println("Nachricht: " + message);
+                // try-catch-Block für die AES-Verschlüsselung
                 try {
+                    // AES-Verschlüsselung
                     System.out.println("Verschlüsselte " +
                             "Nachricht: " + AESen(message, key));
                 } catch (Exception e) {
+                    // Fehlermeldung ausgeben
                     System.out.println("Error: " + e.getMessage());
                 }
             }
+            // Modus 2: AES-Verschlüsseln, aber mit einem zufälligen Schlüssel
             case 2 -> {
+                // Ausgaben für den Benutzer
                 System.out.println("Bitte geben Sie einen " +
                         "Klartext ein:");
+                // Klartext einlesen
                 String message = in.nextLine();
+                // Ausgaben für den Benutzer
                 System.out.println("Ihre Eingabe wird nun m. " +
                         "einem zufälligen Schl. verschl..");
+                // SecureRandom-Objekt erstellen
                 SecureRandom random = new SecureRandom();
+                // String für die zulässigen buchstaben erstellen
                 String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWX" +
                         "YZabcdefghijklmnopqrstuvwxyz";
+                // String für den zufälligen Schlüssel erstellen
                 StringBuilder sb = new StringBuilder(16);
+                // Schlüssel generieren
                 for (int i = 0; i < 16; i++)
                     sb.append(AB.charAt(
                             random.nextInt(AB.length())));
                 String key = sb.toString();
+                // try-catch-Block für die AES-Verschlüsselung
                 try {
+                    // AES-Verschlüsselung
                     System.out.println("Verschlüsselte " +
                             "Nachricht: " + AESen(message, key));
+                    // Schlüssel ausgeben
                     System.out.println("Schlüssel: " + key);
                 } catch (Exception e) {
+                    // Fehlermeldung ausgeben
                     System.out.println("Error: " + e.getMessage());
                 }
             }
+            // Modus 3: AES-Entschlüsseln
             case 3 -> {
+                // Ausgaben für den Benutzer
                 System.out.println("Bitte geben Sie einen " +
                         "verschl. Text ein:");
+                // verschl. Text einlesen
                 String message = in.nextLine();
+                // Ausgaben für den Benutzer
                 System.out.println("Bitte geben Sie Ihren" +
                         " Schlüssel ein:");
+                // Schlüssel einlesen
                 String key = in.nextLine();
+                // Nachricht ausgeben
                 System.out.println("Nachricht: " + message);
+                // try-catch-Block für die AES-Entschlüsselung
                 try {
+                    // AES-Entschlüsselung
                     System.out.println("Entschlüsselte " +
                             "Nachricht: " + AESde(message, key));
                 } catch (Exception e) {
+                    // Fehlermeldung ausgeben
                     System.out.println("Error: " + e.getMessage());
                 }
             }
+            // Fehlermeldung ausgeben, wenn der Modus nicht existiert
             default -> System.out.println("Ungültige Eingabe!");
         }
     }
 
     public static String AESen(String message, String key)
             throws Exception {
+        // schlüsselobjekt erstellen
         SecretKeySpec keySpec = new SecretKeySpec
                 (key.getBytes(), "AES");
+        // neues SecureRandom-Objekt erstellen
         SecureRandom prng = new SecureRandom();
+        // byte-Array für den IV erstellen
         byte[] iv = new byte[16];
+        // IV generieren
         prng.nextBytes(iv);
+        // iv-Objekt erstellen
         IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        // cipher-Objekt erstellen
         Cipher cipher = Cipher.getInstance
                 ("AES/CBC/PKCS5Padding");
+        // cipher-Objekt initialisieren
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        // byte-Array für den verschlüsselten Text erstellen
         byte[] cipherText = cipher.doFinal(message.getBytes());
+        // byte-Array für den output-String erstellen
         ByteArrayOutputStream os = new ByteArrayOutputStream();
+        // IV in den bytestream schreiben
         os.write(iv);
+        // verschlüsselten Text in den bytestream schreiben
         os.write(cipherText);
+        // output-String encoden und zurückgeben
         Base64.Encoder encoder = Base64.getEncoder();
         return encoder.encodeToString(os.toByteArray());
     }
 
     public static String AESde(String message, String key)
             throws Exception {
+        // schlüsselobjekt erstellen
         SecretKeySpec keySpec = new SecretKeySpec
                 (key.getBytes(), "AES");
+        // byte-Array für IV erstellen
         byte[] iv = new byte[16];
+        // neues Decoder-Objekt erstellen
         Base64.Decoder decoder = Base64.getDecoder();
+        // byte-Array für den verschlüsselten Text erstellen
         byte[] a = decoder.decode(message);
+        // IV aus byte array kopieren ...
         System.arraycopy(a, 0, iv, 0, 16);
+        // und in das neue iv-Objekt schreiben
         IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        // neuen byte array für den bereinigten verschlüsselten Text erstellen
         byte[] newa = new byte[a.length - 16];
+        // bereinigten verschlüsselten Text aus byte array kopieren ...
         System.arraycopy(a, 16, newa, 0, a.length - 16);
         a = newa;
+        // cipher-Objekt erstellen
         Cipher aes = Cipher.getInstance
                 ("AES/CBC/PKCS5Padding");
+        // cipher-Objekt initialisieren
         aes.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        // byte-Array für den entschlüsselten Text erstellen
         byte[] decipheredText = aes.doFinal(a);
+        // output-String encoden und zurückgeben
         return new String(decipheredText);
     }
 }
